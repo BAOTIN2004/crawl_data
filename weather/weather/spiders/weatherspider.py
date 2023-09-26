ef86e773bfc25bd2266077d640e13df1d9869e38
BAOTIN2004 <baotinphamphuoc@gmail.com>
2023-09-26 17:23:28 +0700

    weather

--- weather/weather/spiders/weatherspider.py
+++ weather/weather/spiders/weatherspider.py
@@ -1,16 +1,19 @@
 import scrapy
 from ..items import WeatherItem #Tên của dự án + Item
-
+import time
 class SpiderBasicSpider(scrapy.Spider):
     name = "weatherspider"
     allowed_domains = ["weather.com"]
     start_urls = ["https://weather.com/"]
 
     def parse(self, response):
-        temp= response.xpath('//span[class="CurrentConditions--tempValue--MHmYY"]/text()').get()
+        time.sleep(5)
+        city = response.xpath('//h1[@class="CurrentConditions--location--1YWj_"]/text()').get()
+        temp=reviews = response.xpath('//span[@class="CurrentConditions--tempValue--MHmYY"]/text()').getall()
        # authors = response.xpath('//small[@class="author"]/text()').getall()
        # for review, author in zip(reviews, authors):
        #for review in reviews:
+        
         item = WeatherItem() #Tạo 1 đối tượng chứa dữ liệu
         # temp= response.xpath('//span[@data-testid="TemperatureValue"]/text()').get()
 
@@ -18,4 +21,4 @@ class SpiderBasicSpider(scrapy.Spider):
         # item = WeatherItem()
         item["temp"]= temp
         yield item
-    pass
+        pass
